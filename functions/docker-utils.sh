8e83e89296ae1668d56aa9e58dcb59902d9232a0
dlafrance <david.lafrance@bellmedia.ca>
2021-12-21 11:46:59 -0500

    added function to docker exec on a swarm service

--- functions/docker-utils.sh
+++ functions/docker-utils.sh
@@ -22,6 +22,10 @@ function docker-build-local() {
     docker build --pull --file Dockerfile --tag "${PWD##*/}":local "$@"
 }
 
+function docker-service-exec() {
+    docker exec $( docker service ps -f "desired-state=running" --no-trunc --format "{{.Name}}.{{.ID}}" "$1" ) "${@: 2}"
+}
+
 function docker-service-update() {
     docker service update --force "${DOCKER_STACK_NAME}_${PWD##*/}"
 }
